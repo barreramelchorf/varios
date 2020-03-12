3efafdfb38a49ae4229f5469717be7359b871b82
barreramelchorf <barreramelchorf@gmail.com>
2020-03-12 11:42:44 -0600

    Update createUsers.sh

--- createUsers.sh
+++ createUsers.sh
@@ -2,12 +2,10 @@
 #
 ACTIVEAMBARIHOST=headnodehost
 PORT=8080
-usuario=usrsistemasdev
-password="Liverpool123:"
 
-curl -iv -u admindev:Liverpool123: -H "X-Requested-By: ambari" -X POST -d  '{"Users/user_name": "$usuario", "Users/password":  "$password", "Users/active": true, "Users/admin": false }' http://$ACTIVEAMBARIHOST:$PORT/api/v1/users
+curl -iv -u admindev:Liverpool123: -H "X-Requested-By: ambari" -X POST -d  '{"Users/user_name": "usrsistemasdev", "Users/password":  "Liverpool123:", "Users/active": true, "Users/admin": false }' http://$ACTIVEAMBARIHOST:$PORT/api/v1/users
 
 echo "Creating user systemsuser"
+password="Liverpool123:"
 pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-sudo useradd -m -p $pass $usuario
- 
+sudo useradd -m -p $pass usrsistemasdev
