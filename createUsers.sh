c4234ad1962f22fb4ccea76cf18484537326b370
barreramelchorf <barreramelchorf@gmail.com>
2020-03-12 09:00:45 -0600

    Update createUsers.sh

--- createUsers.sh
+++ createUsers.sh
@@ -6,7 +6,7 @@ PORT=8080
 echo "Creating user systemsuser"
 password="Liverpool123:"
 pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-sudo useradd -m -p $pass fbarream
+sudo useradd -m -p $pass fbarreram
 
 curl -iv -u admindev:Liverpool123: -H "X-Requested-By: ambari" -X POST -d  '{"Users/user_name": "fbarreram", "Users/password":  "Liverpool123:", "Users/active": true, "Users/admin": false }' http://$ACTIVEAMBARIHOST:$PORT/api/v1/users
  
